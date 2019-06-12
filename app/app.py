538cc6c0504dc1a40a08abaea72190457c3e6ac4
oukaja <oukaja.mehdi@gmail.com>
2019-06-12 11:19:46 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,5 +10,4 @@ def index():
   """
 
 if __name__ == "__main__":  
-    app.run(debug=True, host='0.0.0.0')
-    # app.run(debug=True, host='0.0.0.0', port=80)
+  app.run(host='0.0.0.0', debug=True, port=80)
